b01ae70a2f0dadf73d4d686f909584bdb2abaae5
Joanka <joannawrzolek@gmail.com>
2017-03-06 23:24:19 +0100

    Added new method in class Konto

--- Bank/Bank/Konto.cs
+++ Bank/Bank/Konto.cs
@@ -18,12 +18,35 @@ namespace Bank
         private decimal saldo = 0;
         private int pin = 0;
 
-        private bool sprawdzPin(int pin)
+        private bool SprawdzPin(int pin)
         {
             if (this.pin == pin)
                 return true;
             return false;
+        }
 
+        public void DokonajWplaty(decimal kwota)
+        {
+            if(kwota < 0)
+                throw new ArgumentException("Wpłacana kwota nie może być mniejsza od zera");
+            saldo += kwota;
+        }
+
+        public string DokonajWyplaty(decimal kwota, int pin)
+        {
+            if (!SprawdzPin(pin) || saldo < kwota)
+                return "Operacja anulowana";
+            saldo -= kwota;
+            return "Operacje zakonczono pomyślnie";
+        }
+
+        public bool ZmienPin(int nowy, int stary)
+        {
+            if(SprawdzPin(stary)){
+                pin = nowy;
+                return true;
+            }
+            return false;
         }
     }
 }
