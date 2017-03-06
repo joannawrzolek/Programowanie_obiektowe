8e7ea4ad5ea212dc4a3b71646e342d15fcaa62d6
Joanka <joannawrzolek@gmail.com>
2017-03-06 23:09:39 +0100

    Added projekt

---
+++ Bank/Bank/Konto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    class Osoba
+    {
+        public string Imie;
+        public string Nazwisko;
+
+    }
+    class Konto
+    {
+        public Osoba Wlasciciel;
+        private decimal saldo = 0;
+        private int pin = 0;
+
+        private bool sprawdzPin(int pin)
+        {
+            if (this.pin == pin)
+                return true;
+            return false;
+
+        }
+    }
+}
