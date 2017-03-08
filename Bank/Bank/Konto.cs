a0e6e9f562d5ec922e78e3d2bc444816f3c3821c
Joanka <joannawrzolek@gmail.com>
2017-03-08 15:57:56 +0100

    Added method in Konto

--- Bank/Bank/Konto.cs
+++ Bank/Bank/Konto.cs
@@ -8,16 +8,27 @@ namespace Bank
 {
     class Osoba
     {
+        /// <summary>
+        /// Set class Osoba data
+        /// </summary>
         public string Imie;
         public string Nazwisko;
 
     }
     class Konto
     {
+        /// <summary>
+        /// Set class Konto data
+        /// </summary>
         public Osoba Wlasciciel;
         private decimal saldo = 0;
         private int pin = 0;
 
+        /// <summary>
+        /// Method to verify if PIN is correct
+        /// </summary>
+        /// <param name="pin"></param>
+        /// <returns></returns>
         private bool SprawdzPin(int pin)
         {
             if (this.pin == pin)
@@ -25,6 +36,10 @@ namespace Bank
             return false;
         }
 
+        /// <summary>
+        /// Method set up the payment on account
+        /// </summary>
+        /// <param name="kwota"></param>
         public void DokonajWplaty(decimal kwota)
         {
             if(kwota < 0)
@@ -32,6 +47,12 @@ namespace Bank
             saldo += kwota;
         }
 
+        /// <summary>
+        /// Method set up the withdraw from the account
+        /// </summary>
+        /// <param name="kwota"></param>
+        /// <param name="pin"></param>
+        /// <returns></returns>
         public string DokonajWyplaty(decimal kwota, int pin)
         {
             if (!SprawdzPin(pin) || saldo < kwota)
@@ -40,6 +61,12 @@ namespace Bank
             return "Operacje zakonczono pomyślnie";
         }
 
+        /// <summary>
+        /// Method to changed to PIN. Returns bool.
+        /// </summary>
+        /// <param name="nowy"></param>
+        /// <param name="stary"></param>
+        /// <returns></returns>
         public bool ZmienPin(int nowy, int stary)
         {
             if(SprawdzPin(stary)){
@@ -47,6 +74,18 @@ namespace Bank
                 return true;
             }
             return false;
+        }
+
+        /// <summary>
+        /// Method returns acccount balance, if Pin is corects
+        /// </summary>
+        /// <param name="pin"></param>
+        /// <returns></returns>
+        public string PobierzInformacje (int pin)
+        {
+            if (SprawdzPin(pin))
+                return string.Format("Pan(i) {0} {1} posiada na koncie {2}", Wlasciciel.Imie, Wlasciciel.Nazwisko, saldo);
+            return "Brak dostepu ";
         }
     }
 }
--- Bank/Bank/Program.cs
+++ Bank/Bank/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/// <summary>
+/// The App is dedicated to a Banks
+/// There is two class and variables initialized value of zero.
+/// The withdraw from the account and payment on account are possible only if the PIN is correct.
+/// </summary>
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +16,33 @@ namespace Bank
     {
         static void Main(string[] args)
         {
+            Konto[] konta = new Konto[2];
+            konta[0] = new Konto();
+            konta[1] = new Konto();
+
+            konta[0].Wlasciciel = new Osoba();
+            konta[1].Wlasciciel = new Osoba();
+
+            konta[0].Wlasciciel.Imie = "Jan";
+            konta[0].Wlasciciel.Nazwisko = "Kowalski";
+            konta[1].Wlasciciel.Imie = "Ala";
+            konta[1].Wlasciciel.Nazwisko = "Nowak";
+
+            Console.WriteLine("Proba zmiany PIN: {0}", konta[0].ZmienPin(1111, 0));
+            Console.WriteLine("Proba zmiany PIN: {0}", konta[1].ZmienPin(2222, 0));
+
+            Console.WriteLine("Dokonujemy wpłat:");
+            konta[0].DokonajWplaty(1200);
+            konta[1].DokonajWplaty(2200);
+
+            Console.WriteLine("Dokonyjemy wyplaty: {0}", konta[0].DokonajWyplaty(300, 1111));
+            Console.WriteLine("Dokonyjemy wyplaty: {0}", konta[0].DokonajWyplaty(3000, 1111));
+            Console.WriteLine("Dokonyjemy wyplaty: {0}", konta[1].DokonajWyplaty(200, 2222));
+            Console.WriteLine("Dokonyjemy wyplaty: {0}", konta[1].DokonajWyplaty(200, 1111));
+
+            Console.WriteLine("Informacje o koncie: {0}", konta[0].PobierzInformacje(1111));
+            Console.WriteLine("Informacje o koncie: {0}", konta[1].PobierzInformacje(2222));
+            Console.WriteLine("Informacje o koncie: {0}", konta[1].PobierzInformacje(0));
         }
     }
 }
